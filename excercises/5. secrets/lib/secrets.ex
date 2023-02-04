5e584934c82e83a351dba4b9f0ab45d352a89bc2
Torean Joel <toreanjoel@gmail.com>
2023-02-04 23:54:26 +0200

    Update secrets.ex

--- excercises/5. secrets/lib/secrets.ex
+++ excercises/5. secrets/lib/secrets.ex
@@ -15,24 +15,28 @@ defmodule Secrets do
   adder = Secrets.secret_add(2)
   adder.(2)
   # => 4
+
   2. Create a subtractor
   Implement Secrets.secret_subtract/1. It should return a function which takes one argument and subtracts the secret passed in to secret_subtract from that argument.
 
   subtractor = Secrets.secret_subtract(2)
   subtractor.(3)
   # => 1
+
   3. Create a multiplier
   Implement Secrets.secret_multiply/1. It should return a function which takes one argument and multiplies it by the secret passed in to secret_multiply.
 
   multiplier = Secrets.secret_multiply(7)
   multiplier.(3)
   # => 21
+
   4. Create a divider
   Implement Secrets.secret_divide/1. It should return a function which takes one argument and divides it by the secret passed in to secret_divide.
 
   divider = Secrets.secret_divide(3)
   divider.(32)
   # => 10
+
   Make use of integer division so the output is compatible with the other functions' expected input.
 
   5. Create an "and"-er
@@ -41,12 +45,14 @@ defmodule Secrets do
   ander = Secrets.secret_and(1)
   ander.(2)
   # => 0
+
   6. Create an "xor"-er
   Implement Secrets.secret_xor/1. It should return a function which takes one argument and performs a bitwise xor operation on it and the secret passed in to secret_xor.
 
   xorer = Secrets.secret_xor(1)
   xorer.(3)
   # => 2
+
   7. Create a function combiner
   Implement Secrets.secret_combine/2. It should return a function which takes one argument and applies to it the two functions passed in to secret_combine in order.
 
