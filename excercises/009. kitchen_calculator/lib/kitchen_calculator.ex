5f4a5a7637cfd050f008cc2adc09b56edcce7276
Torean Joel <toreanjoel@Toreans-MacBook-Pro.local>
2023-02-05 03:34:01 +0200

    Excercism 9 (test and answers)

--- excercises/009. kitchen_calculator/lib/kitchen_calculator.ex
+++ excercises/009. kitchen_calculator/lib/kitchen_calculator.ex
@@ -23,21 +23,114 @@ defmodule KitchenCalculator do
   2. Convert the volume-pair to milliliters
   Implement the KitchenCalculator.to_milliliter/1 function. Given a volume-pair tuple, it should convert the volume to milliliters using the conversion chart.
 
-  Use multiple function clauses and pattern matching to create the functions for each unit. The atoms used to denote each unit are: :cup, :fluid_ounce, :teaspoon, :tablespoon, :milliliter. Return the result of the conversion wrapped in a tuple.
+  Use multiple function clauses and pattern matching to create the functions for each unit.
+  The atoms used to denote each unit are:
+  :cup, :fluid_ounce, :teaspoon, :tablespoon, :milliliter.
+  Return the result of the conversion wrapped in a tuple.
 
   KitchenCalculator.to_milliliter({:cup, 2.5})
   # => {:milliliter, 600.0}
+
   3. Convert the milliliter volume-pair to another unit
-  Implement the KitchenCalculator.from_milliliter/2 function. Given a volume-pair tuple and the desired unit, it should convert the volume to the desired unit using the conversion chart.
+  Implement the KitchenCalculator.from_milliliter/2 function.
+  Given a volume-pair tuple and the desired unit,
+  it should convert the volume to the desired unit using the conversion chart.
 
   Use multiple function clauses and pattern matching to create the functions for each unit. The atoms used to denote each unit are: :cup, :fluid_ounce, :teaspoon, :tablespoon, :milliliter
 
   KitchenCalculator.from_milliliter({:milliliter, 1320.0}, :cup)
   # => {:cup, 5.5}
+
   4. Convert from any unit to any unit
   Implement the KitchenCalculator.convert/2 function. Given a volume-pair tuple and the desired unit, it should convert the given volume to the desired unit.
 
   KitchenCalculator.convert({:teaspoon, 9.0}, :tablespoon)
   # => {:tablespoon, 3.0}
   """
+
+  # ml converted to one of the other metric conversions
+  @to_ml %{
+    milliliter: 1,
+    cup: 240,
+    fluid_ounce: 30,
+    teaspoon: 5,
+    tablespoon: 15,
+  }
+
+  @doc """
+    Get the value off a passed unit value tuple
+
+    ## Example
+      iex> KitchenCalculator.get_volume({:cup, 2.0})
+      2.0
+  """
+  def get_volume({_unit, value}) do
+    value
+  end
+
+  @doc """
+    Convert any unit to milliliter
+
+    ## Example
+      iex> KitchenCalculator.to_milliliter({:cup, 2.5})
+      {:milliliter, 600.0}
+
+      iex> KitchenCalculator.to_milliliter({:fluid_ounce, 2.5})
+      {:milliliter, 75.0}
+
+      iex> KitchenCalculator.to_milliliter({:teaspoon, 2.5})
+      {:milliliter, 12.5}
+
+      iex> KitchenCalculator.to_milliliter({:tablespoon, 2.5})
+      {:milliliter, 37.5}
+
+      iex> KitchenCalculator.to_milliliter({:milliliter, 2.5})
+      {:milliliter, 2.5}
+
+
+  """
+  def to_milliliter({unit, value}) when unit === :cup,  do: {:milliliter, value * @to_ml[unit]}
+  def to_milliliter({unit, value}) when unit === :fluid_ounce,  do: {:milliliter, value * @to_ml[unit]}
+  def to_milliliter({unit, value}) when unit === :teaspoon,  do: {:milliliter, value * @to_ml[unit]}
+  def to_milliliter({unit, value}) when unit === :tablespoon,  do: {:milliliter, value * @to_ml[unit]}
+  def to_milliliter({unit, value}) when unit === :milliliter,  do: {:milliliter, value * @to_ml[unit]}
+
+  @doc """
+    Convert any unit from milliliter
+
+    ## Example
+      iex> KitchenCalculator.from_milliliter({:milliliter, 600}, :cup)
+      {:cup, 2.5}
+
+      iex> KitchenCalculator.from_milliliter({:milliliter, 600}, :fluid_ounce)
+      {:fluid_ounce, 20.0}
+
+      iex> KitchenCalculator.from_milliliter({:milliliter, 600}, :teaspoon)
+      {:teaspoon, 120.0}
+
+      iex> KitchenCalculator.from_milliliter({:milliliter, 600}, :tablespoon)
+      {:tablespoon, 40.0}
+
+      iex> KitchenCalculator.from_milliliter({:milliliter, 600}, :milliliter)
+      {:milliliter, 600.0}
+
+
+  """
+  def from_milliliter({ml_unit, value}, unit_to) when unit_to === :cup,  do: {unit_to, value / @to_ml[unit_to]}
+  def from_milliliter({ml_unit, value}, unit_to) when unit_to === :fluid_ounce,  do: {unit_to, value / @to_ml[unit_to]}
+  def from_milliliter({ml_unit, value}, unit_to) when unit_to === :teaspoon,  do: {unit_to, value / @to_ml[unit_to]}
+  def from_milliliter({ml_unit, value}, unit_to) when unit_to === :tablespoon,  do: {unit_to, value / @to_ml[unit_to]}
+  def from_milliliter({ml_unit, value}, unit_to) when unit_to === :milliliter,  do: {unit_to, value / @to_ml[unit_to]}
+
+  @doc """
+    Convert from one value to another
+
+    ## Example
+      iex> KitchenCalculator.convert({:teaspoon, 9.0}, :tablespoon)
+      {:tablespoon, 3.0}
+  """
+  def convert({unit_from, value}, unit_to) do
+    {_, value} = to_milliliter({unit_from, value})
+    {unit_to, value / @to_ml[unit_to]}
+  end
 end
--- excercises/009. kitchen_calculator/test/kitchen_calculator_test.exs
+++ excercises/009. kitchen_calculator/test/kitchen_calculator_test.exs
@@ -2,4 +2,58 @@ defmodule KitchenCalculatorTest do
   use ExUnit.Case
   doctest KitchenCalculator
 
+  @get_colume 12.0
+  @to_ml_value 2.5
+  @from_ml_value 600.0
+
+  test "Get value from tuple volume" do
+    assert KitchenCalculator.get_volume({:cup, @get_colume}) === @get_colume
+  end
+
+  test "Convert from cup, to milliliter" do
+    assert KitchenCalculator.to_milliliter({:cup, @to_ml_value}) === {:milliliter, 600.0}
+  end
+
+  test "Convert from fluid_ounce, to milliliter" do
+    assert KitchenCalculator.to_milliliter({:fluid_ounce, @to_ml_value}) === {:milliliter, 75.0}
+  end
+
+  test "Convert from teaspoon, to milliliter" do
+    assert KitchenCalculator.to_milliliter({:teaspoon, @to_ml_value}) === {:milliliter, 12.5}
+  end
+
+  test "Convert from tablespoon, to milliliter" do
+    assert KitchenCalculator.to_milliliter({:tablespoon, @to_ml_value}) === {:milliliter, 37.5}
+  end
+
+  test "Convert from milliliter, to milliliter" do
+    assert KitchenCalculator.to_milliliter({:milliliter, @to_ml_value}) === {:milliliter, @to_ml_value}
+  end
+
+  test "Convert to cup, from milliliter" do
+    assert KitchenCalculator.from_milliliter({:milliliter, @from_ml_value}, :cup) === {:cup, 2.5}
+  end
+
+  test "Convert to fluid_ounce, from milliliter" do
+    assert KitchenCalculator.from_milliliter({:milliliter, @from_ml_value}, :fluid_ounce) === {:fluid_ounce, 20.0}
+  end
+
+  test "Convert to teaspoon, from milliliter" do
+    assert KitchenCalculator.from_milliliter({:milliliter, @from_ml_value}, :teaspoon) === {:teaspoon, 120.0}
+  end
+
+  test "Convert to tablespoon, from milliliter" do
+    assert KitchenCalculator.from_milliliter({:milliliter, @from_ml_value}, :tablespoon) === {:tablespoon, 40.0}
+  end
+
+  test "Convert to milliliter, from milliliter" do
+    assert KitchenCalculator.from_milliliter({:milliliter, @from_ml_value}, :milliliter) === {:milliliter, @from_ml_value}
+  end
+
+  test "Convert to UNIT, from UNIT" do
+    teaspoon = 9.0
+    tablespoon = 3.0
+    assert KitchenCalculator.convert({:teaspoon, teaspoon}, :tablespoon) === {:tablespoon, tablespoon}
+  end
+
 end
