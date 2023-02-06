a0f867fd12868c74b18dc45430480c6d11ca27c1
Torean Joel <toreanjoel@Toreans-MacBook-Pro.local>
2023-02-06 19:23:29 +0200

    Excercism 11 (test and answers)

--- excercises/011. bird_count/lib/bird_count.ex
+++ excercises/011. bird_count/lib/bird_count.ex
@@ -19,7 +19,9 @@ defmodule BirdCount do
 
   BirdCount.increment_day_count([4, 0, 2])
   # => [5, 0, 2]
+
   3. Check if there was a day with no visiting birds
+
   Implement the BirdCount.has_day_without_birds?/1 function. It should take a list of daily bird counts. It should return true if there was at least one day when no birds visited the garden, and false otherwise.
 
   BirdCount.has_day_without_birds?([2, 0, 4])
@@ -27,11 +29,13 @@ defmodule BirdCount do
 
   BirdCount.has_day_without_birds?([3, 8, 1, 5])
   # => false
+
   4. Calculate the total number of visiting birds
   Implement the BirdCount.total/1 function. It should take a list of daily bird counts and return the total number that visited your garden since you started collecting the data.
 
   BirdCount.total([4, 0, 9, 0, 5])
   # => 18
+
   5. Calculate the number of busy days
   Some days are busier than others. A busy day is one where five or more birds have visited your garden.
 
@@ -40,4 +44,71 @@ defmodule BirdCount do
   BirdCount.busy_days([4, 5, 0, 0, 6])
   # => 2
   """
+
+  @doc """
+    Return the first item in the list, if empty list return nothing
+
+    ## Examples
+      iex> BirdCount.today([])
+      nil
+
+      iex> BirdCount.today([3, 5, 4])
+      3
+  """
+  def today([]), do: nil
+  def today([today | _rest]), do: today
+
+  @doc """
+    Incremnt the first item in the list by 1, return 1 index list item as [1] otherwise
+
+    ## Exmaples
+      iex> BirdCount.increment_day_count([])
+      [1]
+
+      iex> BirdCount.increment_day_count([4, 3, 7])
+      [5, 3, 7]
+  """
+  def increment_day_count([]), do: [1]
+  def increment_day_count([today | rest]), do: [today + 1 | rest]
+
+  @doc """
+    Check if there was a day with 0 as a value, return false otherwise
+
+    ## Exmaples
+      iex> BirdCount.has_day_without_birds?([])
+      false
+
+      iex> BirdCount.has_day_without_birds?([4, 5, 0, 1])
+      true
+  """
+  def has_day_without_birds?([]), do: false # check if the list is empty
+  def has_day_without_birds?([0 | _rest]), do: true # check if the head is 0 -> break out of recursion
+  def has_day_without_birds?([today | rest]), do: has_day_without_birds?(rest) # try again until we checked entire list
+
+  @doc """
+    Add up all values in a list
+
+    ## Exmaples
+      iex> BirdCount.total([])
+      0
+
+      iex> BirdCount.total([4, 5, 0, 1])
+      10
+  """
+  def total([]), do: 0
+  def total([today | rest]), do: today + total(rest)
+
+  @doc """
+    Check if any of the items in the list are more than or equal to 5, increment a count of them
+
+    ## Exmaples
+      iex> BirdCount.busy_days([])
+      0
+
+      iex> BirdCount.busy_days([4, 5, 0, 1])
+      1
+  """
+  def busy_days([]), do: 0
+  def busy_days([today | rest]) when today >= 5, do: busy_days(rest) + 1
+  def busy_days([_today | rest]), do: busy_days(rest)
 end
--- excercises/011. bird_count/test/bird_count_test.exs
+++ excercises/011. bird_count/test/bird_count_test.exs
@@ -2,4 +2,43 @@ defmodule BirdCountTest do
   use ExUnit.Case
   doctest BirdCount
 
+  test "get first item in list - empty list" do
+    assert BirdCount.today([]) === nil
+  end
+
+  test "get first item in list" do
+    assert BirdCount.today([4, 3, 5]) === 4
+  end
+
+  test "Increment first item by 1 - empty list" do
+    assert BirdCount.increment_day_count([]) === [1]
+  end
+
+  test "Increment first item by 1" do
+    assert BirdCount.increment_day_count([4, 3, 5]) === [5, 3, 5]
+  end
+
+  test "Check any item equal to zero - empty list" do
+    assert BirdCount.has_day_without_birds?([]) === false
+  end
+
+  test "Check any item equal to zero - true" do
+    assert BirdCount.has_day_without_birds?([1, 4, 5, 0]) === true
+  end
+
+  test "Work out total of all items in list - empty list" do
+    assert BirdCount.total([]) === 0
+  end
+
+  test "Work out total of all items in list" do
+    assert BirdCount.total([1, 4, 5, 0]) === 10
+  end
+
+  test "Check indice and increment a count if the value is more or equal to 5 - empty list" do
+    assert BirdCount.busy_days([]) === 0
+  end
+
+  test "Check indice and increment a count if the value is more or equal to 5" do
+    assert BirdCount.busy_days([5, 4, 7, 2, 2]) === 2
+  end
 end
