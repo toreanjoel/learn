81ce960c9239fcf35813599d0d6bca0be8184746
Torean Joel <toreanjoel@Toreans-MacBook-Pro.local>
2023-02-15 16:20:20 +0200

    Excercism 18 - 19 (tests)

--- excercises/018. wine_cellar/test/wine_cellar_test.exs
+++ excercises/018. wine_cellar/test/wine_cellar_test.exs
@@ -1,4 +1,48 @@
 defmodule WineCellarTest do
   use ExUnit.Case
   doctest WineCellar
+
+  test "Get color explain" do
+    assert WineCellar.explain_colors === [
+      white: "Fermented without skin contact.",
+      red: "Fermented with skin contact using dark-colored grapes.",
+      rose: "Fermented with some skin contact, but not enough to qualify as a red wine."
+    ]
+  end
+
+  test "Filter against key" do
+    cellar =  [
+      red: {"name #1", 2000, "country 1"},
+      green: {"name #2", 1993, "country 2"},
+      blue: {"name #3", 1560, "country 3"},
+      green: {"name #4", 1992, "country 4"}
+    ]
+
+    assert WineCellar.filter(cellar, :green) ===
+      [{"name #2", 1993, "country 2"}, {"name #4", 1992, "country 4"}]
+  end
+
+  test "Filter against year" do
+    cellar =  [
+      red: {"name #1", 2000, "country 1"},
+      green: {"name #2", 1993, "country 2"},
+      blue: {"name #3", 1560, "country 3"},
+      green: {"name #4", 1992, "country 4"}
+    ]
+
+    assert WineCellar.filter(cellar, :green, year: 1993) ===
+      [{"name #2", 1993, "country 2"}]
+  end
+
+  test "Filter against country" do
+    cellar =  [
+      red: {"name #1", 2000, "country 1"},
+      green: {"name #2", 1993, "country 2"},
+      blue: {"name #3", 1560, "country 3"},
+      green: {"name #4", 1992, "country 4"}
+    ]
+
+    assert WineCellar.filter(cellar, :blue, country: "country 3") ===
+      [{"name #3", 1560, "country 3"}]
+  end
 end
--- excercises/019. dna_encoding/lib/dna_encoding.ex
+++ excercises/019. dna_encoding/lib/dna_encoding.ex
@@ -3,35 +3,49 @@ defmodule DnaEncoding do
   Documentation for `DnaEncoding`.
 
   Instructions
-In your DNA research lab, you have been working through various ways to compress your research data to save storage space. One teammate suggests converting the DNA data to a binary representation:
+  In your DNA research lab, you have been working through various ways to compress your research data to save storage space.
+  One teammate suggests converting the DNA data to a binary representation:
 
-Nucleic Acid	Code
-a space	0000
-A	0001
-C	0010
-G	0100
-T	1000
-You ponder this, as it will potentially halve the required data storage costs, but at the expense of human readability. You decide to write a module to encode and decode your data to benchmark your savings.
+  Nucleic Acid	Code
+  a space	0000
+  A	0001
+  C	0010
+  G	0100
+  T	1000
 
-1. Encode nucleic acid to binary value
-Implement encode_nucleotide/1 to accept the code point for the nucleic acid and return the integer value of the encoded code.
+  You ponder this, as it will potentially halve the required data storage costs, but at the expense of human readability.
+  You decide to write a module to encode and decode your data to benchmark your savings.
 
-DNA.encode_nucleotide(?A)
-# => 0b0001
-2. Decode the binary value to the nucleic acid
-Implement decode_nucleotide/1 to accept the integer value of the encoded code and return the code point for the nucleic acid.
+  1. Encode nucleic acid to binary value
+  Implement encode_nucleotide/1 to accept the code point for the nucleic acid and return the integer value of the encoded code.
 
-DNA.decode_nucleotide(0b0001)
-# => ?A
-3. Encode a DNA charlist
-Implement encode/1 to accept a charlist representing nucleic acids and gaps and return a bitstring of the encoded data.
+  DNA.encode_nucleotide(?A)
+  # => 0b0001
 
-DNA.encode('AC GT')
-# => <<18, 4, 8::size(4)>>
-4. Decode a DNA bitstring
-Implement decode/1 to accept a bitstring representing nucleic acids and gaps and return the decoded data as a charlist.
+  2. Decode the binary value to the nucleic acid
+  Implement decode_nucleotide/1 to accept the integer value of the encoded code and return the code point for the nucleic acid.
 
-DNA.decode(<<132, 2, 1::size(4)>>)
-# => 'TG CA'
+  DNA.decode_nucleotide(0b0001)
+  # => ?A
+
+  3. Encode a DNA charlist
+  Implement encode/1 to accept a charlist representing nucleic acids and gaps and return a bitstring of the encoded data.
+
+  DNA.encode('AC GT')
+  # => <<18, 4, 8::size(4)>>
+
+  4. Decode a DNA bitstring
+  Implement decode/1 to accept a bitstring representing nucleic acids and gaps and return the decoded data as a charlist.
+
+  DNA.decode(<<132, 2, 1::size(4)>>)
+  # => 'TG CA'
   """
+
+  def encode_nucleotide(acid) do
+    acid
+  end
+
+  def decode_nucleotide(binary) do
+    binary
+  end
 end
